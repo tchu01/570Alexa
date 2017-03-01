dc8d775a85583adffb6b303201ab0e0811994732
Timothy Chu <tchu01@calpoly.edu>
2017-03-01 04:58:55 -0800

    need to finish parsing _data files

--- database.py
+++ database.py
@@ -1,27 +1,33 @@
 import json
 import requests
+import os
 
 
 def run():
     cl = get_countries_list()
+    print(len(cl))
     a3c_to_country = a3c_to_country_dict(cl)
 
     for country, country_a2c, country_a3c in cl:
+        data = {}
+
         file = 'data/' + str(country_a2c) + '.json'
         print(file)
         with open(file, 'w') as outfile:
             url = 'https://restcountries.eu/rest/v2/alpha/' + country_a2c
             response = requests.get(url)
-            json_data = json.loads(response.content.decode('utf-8'))
+            json_data = json.loads(response.content.decode('utf-8', 'ignore'))
 
-            data = {}
+            data['country'] = country
             data['capital'] = json_data['capital']
             data['population'] = json_data['population']
             data['region'] = json_data['region']
-            data['latlng'] = json_data['latlng']
+            data['latlng'] = str(json_data['latlng'][0]) + ', ' + str(json_data['latlng'][1])
 
             borders = json_data['borders']
-            if len(borders) == 1:
+            if len(borders) == 0:
+                str_borders = "none"
+            elif len(borders) == 1:
                 str_borders = borders[0]
             elif len(borders) == 2:
                 str_borders = borders[0] + " and " + borders[1]
@@ -34,13 +40,46 @@ def run():
 
             data['borders'] = str_borders
 
+            languages = json_data['languages']
+            languages = [lang_dict['name'] for lang_dict in languages]
+            if len(languages) == 0:
+                str_languages = "none"
+            elif len(languages) == 1:
+                str_languages = languages[0]
+            elif len(languages) == 2:
+                str_languages = languages[0] + " and " + languages[1]
+            else:
+                str_languages = ', '.join(languages[:-1])
+                str_languages = str_languages + ", and " + languages[-1]
 
-            json.dump(data, outfile)
+            data['languages'] = str_languages
 
-        break
+        factbook_file = '_data/' + str(country_a2c) + '.json'
+        if os.path.isfile(factbook_file) == True:
+            with open(factbook_file, encoding='utf-8') as reader:
+                json_data = json.load(reader)
 
+                data['area'] = 0
+                data['land'] = 0
+                data['water'] = 0
+                data['climate'] = 0
+                data['median_age_male'] = 0
+                data['median_age_female'] = 0
+                data['life_expectancy_male'] = 0
+                data['life_expectancy_female'] = 0
+                data['literacy_male'] = 0
+                data['literacy_female'] = 0
+                data['unemployment'] = 0
+                data['poverty_line'] = 0
+        else:
+            print("Warning, no factbook json for...")
+            print(country)
+            print(country_a2c)
+            print(country_a3c)
+            print()
 
-
+        # json.dump(data, outfile)
+        # break
 
 
 def get_countries_list():
