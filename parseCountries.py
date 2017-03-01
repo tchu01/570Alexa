fb90e37f337b0aba163a08e97873d36a5390e23c
Timothy Chu <tchu01@calpoly.edu>
2017-02-28 20:01:19 -0800

    Initial simple questions working

--- parseCountries.py
+++ parseCountries.py
@@ -8,20 +8,20 @@ def main():
     nl = getNewsList()
     relevant = []
     for a in nl:
-        # print a
+        # print(a)
         found = False
-        for c in cl:
+        for c, a2c in cl:
             if c in a:
                 print("FOUND: " + str(c))
                 # print("FROM: " + str(a))
-                relevant.append(c)
+                relevant.append((c, a2c))
                 found = True
         if not found:
             # print("Could not find a country :<")
             # print("\n")
             pass
 
-    qa = generate_question(relevant)
+    question, answer = generate_question(relevant)
 
 
 def getNewsList():
@@ -51,7 +51,7 @@ def getCountriesList():
     jsonData = json.loads(response.content.decode("utf-8"))
     for country in jsonData:
         # print country['name']
-        countryList.add(country['name'])
+        countryList.add((country['name'], country['alpha2Code']))
 
     # second pass
     url = 'https://restcountries.eu/rest/v2/all'
@@ -59,13 +59,14 @@ def getCountriesList():
     jsonData = json.loads(response.content.decode("utf-8"))
     for country in jsonData:
         # print country['name']
-        countryList.add(country['name'])
+        countryList.add((country['name'], country['alpha2Code']))
 
     return countryList
 
 
 def generate_question(relevant):
-    if random.random() >= 0.2:
+    print()
+    if 0.1 >= 0.2:
         with open('factbook_questions.json') as data_file:
             factbook_questions = json.load(data_file)
         print(len(factbook_questions))
@@ -73,16 +74,67 @@ def generate_question(relevant):
     else:
         with open('simple_questions.json') as data_file:
             simple_questions = json.load(data_file)
-        print(len(simple_questions))
 
-        num_countries = random.randint(1,2)
-        if num_countries == 1:
-            country = random.choice(relevant)
-        else:
-            country1 = random.choice(relevant)
-            country2 = country1
-            while country1 == country2:
-                country2 = random.choice(relevant)
+        keys = list(simple_questions.keys())
+        choice = random.choice(keys)
+
+        question = simple_questions[choice][0]
+        answer = simple_questions[choice][1]
+
+        country, country_a2c = random.choice(relevant)
+        print(choice)
+        print(country)
+        print(country_a2c)
+
+        question = question.replace("#COUNTRY#", country)
+        answer = answer.replace("#COUNTRY#", country)
+
+        if "#COUNTRY2#" not in question:
+            url = "https://restcountries.eu/rest/v2/alpha/" + country_a2c
+            response = requests.get(url)
+            jsonData = json.loads(response.content.decode("utf-8"))
+            val = jsonData[choice]
+            answer = answer.replace("#ANSWER#", str(val))
+
+        elif "#COUNTRY2#" in question:
+            country2 = country
+            country2_a2c = country_a2c
+            while country == country2:
+                country2, country2_a2c = random.choice(relevant)
+
+            print(country2)
+
+            question = question.replace("#COUNTRY2#", country2)
+            answer = answer.replace("#COUNTRY2#", country2)
+
+            url = "https://restcountries.eu/rest/v2/alpha/" + country_a2c
+            response = requests.get(url)
+            jsonData = json.loads(response.content.decode("utf-8"))
+            val = jsonData["population"]
+
+            url = "https://restcountries.eu/rest/v2/alpha/" + country2_a2c
+            response = requests.get(url)
+            jsonData = json.loads(response.content.decode("utf-8"))
+            val2 = jsonData["population"]
+
+            print(val)
+            print(val2)
+
+            if val > val2:
+                final = country
+            else:
+                final = country2
+
+            answer = answer.replace("#ANSWER#", final)
+
+
+
+
+
+        print(question)
+        print(answer)
+
+        return question, answer
 
 
 if __name__ == "__main__":
