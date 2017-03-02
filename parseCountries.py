70b85a934d211f89d4c2fb130abf981e983b25bd
Timothy Chu <tchu01@calpoly.edu>
2017-03-02 00:58:20 -0800

    Completed functionality to generate question/answer based on compiled database

--- parseCountries.py
+++ parseCountries.py
@@ -21,7 +21,7 @@ def main():
             # print("\n")
             pass
 
-    question, answer = generate_question(relevant)
+    question, answer = generate_qa(relevant)
 
 
 def getNewsList():
@@ -64,77 +64,69 @@ def getCountriesList():
     return countryList
 
 
-def generate_question(relevant):
-    print()
-    if 0.1 >= 0.2:
-        with open('factbook_questions.json') as data_file:
-            factbook_questions = json.load(data_file)
-        print(len(factbook_questions))
+def generate_qa(relevant):
+    country, country_a2c = random.choice(relevant)
+    print(country)
+    print(country_a2c)
 
-    else:
-        with open('simple_questions.json') as data_file:
-            simple_questions = json.load(data_file)
+    country_file = 'data/' + str(country_a2c) + '.json'
+    with open(country_file) as country_json:
+        country_answers = json.load(country_json)
 
-        keys = list(simple_questions.keys())
-        choice = random.choice(keys)
+        with open('questions.json') as questions_json:
+            possible_questions = json.load(questions_json)
 
-        question = simple_questions[choice][0]
-        answer = simple_questions[choice][1]
+            keys = list(country_answers.keys())
+            keys.remove('country')
+            keys.append('larger_population')
+            keys.append('larger_area')
+            choice = random.choice(keys)
+            print(choice)
 
-        country, country_a2c = random.choice(relevant)
-        print(choice)
-        print(country)
-        print(country_a2c)
+            question = possible_questions[choice][0]
+            answer = possible_questions[choice][1]
 
-        question = question.replace("#COUNTRY#", country)
-        answer = answer.replace("#COUNTRY#", country)
+            question = question.replace("#COUNTRY#", country)
+            answer = answer.replace("#COUNTRY#", country)
 
-        if "#COUNTRY2#" not in question:
-            url = "https://restcountries.eu/rest/v2/alpha/" + country_a2c
-            response = requests.get(url)
-            jsonData = json.loads(response.content.decode("utf-8"))
-            val = jsonData[choice]
-            answer = answer.replace("#ANSWER#", str(val))
-
-        elif "#COUNTRY2#" in question:
-            country2 = country
-            country2_a2c = country_a2c
-            while country == country2:
-                country2, country2_a2c = random.choice(relevant)
-
-            print(country2)
-
-            question = question.replace("#COUNTRY2#", country2)
-            answer = answer.replace("#COUNTRY2#", country2)
-
-            url = "https://restcountries.eu/rest/v2/alpha/" + country_a2c
-            response = requests.get(url)
-            jsonData = json.loads(response.content.decode("utf-8"))
-            val = jsonData["population"]
-
-            url = "https://restcountries.eu/rest/v2/alpha/" + country2_a2c
-            response = requests.get(url)
-            jsonData = json.loads(response.content.decode("utf-8"))
-            val2 = jsonData["population"]
-
-            print(val)
-            print(val2)
-
-            if val > val2:
-                final = country
+            if '#COUNTRY2#' not in question:
+                answer = answer.replace('#ANSWER#', str(country_answers[choice]))
             else:
-                final = country2
+                country2 = country
+                country2_a2c = country_a2c
+                while country == country2:
+                    country2, country2_a2c = random.choice(relevant)
 
-            answer = answer.replace("#ANSWER#", final)
+                print(country2)
+                print(country2_a2c)
 
+                country2_file = 'data/' + str(country2_a2c) + '.json'
+                with open(country2_file) as country2_json:
+                    country2_answers = json.load(country2_json)
 
+                    question = question.replace("#COUNTRY2#", country2)
+                    answer = answer.replace("#COUNTRY2#", country2)
 
+                    if choice == 'larger_population':
+                        choice = 'population'
+                    elif choice == 'larger_area':
+                        choice = 'area'
 
+                    val = country_answers[choice]
+                    val2 = country2_answers[choice]
+                    print(val)
+                    print(val2)
 
-        print(question)
-        print(answer)
+                    if val > val2:
+                        final = country
+                    else:
+                        final = country2
 
-        return question, answer
+                    answer = answer.replace("#ANSWER#", final)
+
+            print(question)
+            print(answer)
+            return question, answer
 
 
 if __name__ == "__main__":
